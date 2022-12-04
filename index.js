cd3fa9924937056f2f678d0283c83cdfdb29db78
hassanabdinasir4 <hassan.hassan@student.moringaschool.com>
2022-12-04 08:39:25 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader= document.createElement('h1')
+console.log('newHeader')
+
+newHeader.id ='victory'
+newHeader.innerHTML = "Hassuke is the champion"
+
+
